ac68a46a91fc3271540ea52a87dcc0e7093f7647
Reham <r_nour30@yahoo.com>
2023-11-30 15:08:31 -0800

    command line

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    ac = sys.argv
+    nums = len(ac) - 1
+    if ac == 0:
+        print("0 arguments.")
+    elif ac == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(nums))
+    for a in range(1, nums + 1):
+        print("{:d}: {:s}".format(a, ac[a]))
