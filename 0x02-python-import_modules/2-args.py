a70cfa631af6c85107e029a3a2de3fc8ff3e06b0
Reham <r_nour30@yahoo.com>
2023-11-30 15:14:19 -0800

    command line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == '__main__':
     import sys
     ac = sys.argv
     nums = len(ac) - 1
-    if ac == 0:
+    if nums == 0:
         print("0 arguments.")
-    elif ac == 1:
+    elif nums == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(nums))
+        print("{:d} arguments:".format(nums))
     for a in range(1, nums + 1):
-        print("{}: {}".format(a, ac[a]))
+        print("{:d}: {:s}".format(a, ac[a]))
