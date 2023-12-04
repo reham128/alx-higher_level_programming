3575487afbaa1b07b3e11d494c32cb2ad399c4d4
Reham <r_nour30@yahoo.com>
2023-12-04 13:53:24 -0800

    mm

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     strlen = len(my_string)
     count = ''
     for a in my_string:
-        if (my_string != 'C' and my_string != 'c'):
+        if (a != 'C' and a != 'c'):
             count = count + a
     return count
