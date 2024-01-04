fb43077dd20b3ea889258303c301c321d333d07c
Reham <r_nour30@yahoo.com>
2024-01-04 13:20:58 -0800

    up

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -59,4 +59,4 @@ class Rectangle:
 
     def __rep__(self):
         '''eturn a string representation of the rectangle'''
-        return ('Rectangle({}, {})'.format(self.__width, self.__height))
+        return ('Rectangle({:d}, {:d})'.format(self.__width, self.__height))
