eb411399b7a277b2ac8cbe9e99b1daf9208c36b9
Reham <r_nour30@yahoo.com>
2024-01-04 10:40:55 -0800

    up

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+'''class definition'''
+
+
 class Square:
+    '''class start'''
 
     def __init__(self, size):
+        '''init definition
+
+        Args:
+        size: side length
+        '''
         self.__size = size
