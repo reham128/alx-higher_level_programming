3ec3c8cd9216d1bd2baaf4f9dd8c2db957e2af96
Reham <r_nour30@yahoo.com>
2024-01-08 03:41:17 -0800

    up

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''empyt class body'''
+
+
+class BaseGeometry:
+    '''class start'''
+    
+    def area(self):
+        '''empyt'''
+        raise Exception("area() is not implemented")
