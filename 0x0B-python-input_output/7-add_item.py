2649add66f0d8a490688136f8c14614e5f2a5b28
Reham <r_nour30@yahoo.com>
2024-01-10 02:40:00 -0800

    up

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''task 7 which using the 2 previous tasks'''
+
+
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = \
+            __import__("6-load_from_json_file").load_from_json_file
+
+    list_o = list(sys.argv[1:])
+    try:
+        data = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        data = []
+    data.extend(list_o)
+    save_to_json_file(data, "add_item.json")
