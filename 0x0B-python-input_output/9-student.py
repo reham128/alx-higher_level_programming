b975b6738be6f0ff04b63f0a5925c7ef8c3d58fc
Reham <r_nour30@yahoo.com>
2024-01-10 21:56:28 -0800

    up

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,12 +2,14 @@
 '''class Student that defines a student'''
 
 
-def __init__(self, first_name, last_name, age):
+class Student:
     '''class body'''
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    def __init__(self, first_name, last_name, age):
+        '''init start'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-def to_json(self):
-    '''dic represent for student in json'''
-    return (self.__dict__)
+    def to_json(self):
+        '''dic represent for student in json'''
+        return (self.__dict__)
