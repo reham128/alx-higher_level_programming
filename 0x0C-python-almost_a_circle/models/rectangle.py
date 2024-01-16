07ae63942283b30890515bbb405bfcc6396c02a5
Reham <r_nour30@yahoo.com>
2024-01-16 01:55:36 -0800

    up

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -63,9 +63,9 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         if type(value) != int:
-            raise TypeError("x must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("y must be > 0")
         self.__y = value
 
     def area(self):
