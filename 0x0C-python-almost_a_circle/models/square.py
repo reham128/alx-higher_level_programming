91df0bba218587d4266d2627420dfeb1d80f3188
Reham <r_nour30@yahoo.com>
2024-01-19 12:22:59 -0800

    up

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,9 +35,8 @@ class Square(Rectangle):
             return
         try:
             self.id = args[0]
-            self.width = args[1]
-            self.height = args[2]
-            self.x = args[3]
-            self.y = args[4]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
         except IndexError:
             pass
