29b59ff5844a4af6cd8c75867384e59a49e510c0
Reham <r_nour30@yahoo.com>
2024-03-04 05:25:58 -0800

    up

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- to insert a new row in the table
 
-INSERT INTO hbtn_0c_0.first_table ('id', 'name') VALUES (89, "Best School");
+INSERT INTO first_table ('id', 'name') VALUES (89, "Best School");
