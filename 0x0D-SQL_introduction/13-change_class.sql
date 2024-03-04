f8a6455b37478d3d3b6f72f15e79c50ac7d8cb8e
Reham <r_nour30@yahoo.com>
2024-03-04 06:05:13 -0800

    up

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+-- to remove all recordes with score <=5 form second_table.
+
+DELETE FROM second_table
+WHERE score<=5
+;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- to compute score avg of all recordes of second_table.
+
+SELECT AVG(score)
+AS average
+FROM second_table
+;
