34da144fe4e97b95e3e0dc3a9119fc96aa293c79
Reham <r_nour30@yahoo.com>
2024-03-04 05:46:28 -0800

    up

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- to create table "second_table" in database hbtn_0c_0.
+
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+-- then, Insert new records in this new table.
+
+INSERT INTO second_table(id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8)
+;
