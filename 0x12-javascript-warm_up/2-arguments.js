f3781797825848cf33c09ea5bd5b120070fd2754
Reham <r_nour30@yahoo.com>
2024-03-11 19:23:41 -0700

    up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const numArg = process.argv.length;
 if (numArg === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (numArg === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
