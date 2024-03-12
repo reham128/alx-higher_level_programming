65376717bb20f0f4cea3e75c318dce4486c5e9f1
Reham <r_nour30@yahoo.com>
2024-03-11 20:10:04 -0700

    up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,9 +4,9 @@ const num = process.argv[2];
 if (num === undefined || isNaN(num)) {
   console.log('Missing size');
 } else {
-	let i = 0;
-	while (i < Number(num)) {
-		console.log('X'.repeat(Number(num)));
-		i++;
-	}
+  let i = 0;
+  while (i < Number(num)) {
+    console.log('X'.repeat(Number(num)));
+    i++;
+  }
 }
