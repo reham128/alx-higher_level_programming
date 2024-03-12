72d7c5d58954bf19b4b482a86240111c46426bec
Reham <r_nour30@yahoo.com>
2024-03-11 19:57:51 -0700

    up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const num = process.argv[2];
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', parseInt(num));
+  console.log('My number:', parseInt(num));
 }
