7f909c9765f7e307235c79599546bf546f39094e
Reham <r_nour30@yahoo.com>
2024-03-17 16:37:27 -0700

    up

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' script that lists all states from the database hbtn_0e_0_usa'''
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_n = sys.argv[4]
+
+    if (len(sys.argv) >= 5):
+        mydb = MySQLdb.connect(host="localhost", port=3306, user=username,
+                               passwd=password, db=database)
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM states WHERE BINARY \
+           name = '{}' ORDER BY".format(state_n)
+    mycursor.execute(sql)
+    states = mycursor.fetchall()
+    for result in states:
+        print(result)
+    mycursor.close()
+    mydb.close()
