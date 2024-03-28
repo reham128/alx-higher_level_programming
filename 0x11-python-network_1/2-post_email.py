49871ecb040ddd8947f48ca2bec8347eb97ba5f0
Reham <r_nour30@yahoo.com>
2024-03-28 16:39:32 -0700

    up

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,9 +9,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = urllib.parse.urlencode({"email": email})
-    data = data.encode('utf-8')
-
-    req =  urllib.request.Request(url, data=data, method='POST')
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+    data = urllib.parse.urlencode({"email": email}).encode('utf-8')
+    requ = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(requ) as res:
+        print(res.read().decode('utf-8'))
